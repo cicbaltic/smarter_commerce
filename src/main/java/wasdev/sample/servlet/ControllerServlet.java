58171806170e975b91c7a375e82d588e81d4cead
Liudis <liudas.bogusas@gmail.com>
2016-04-12 13:29:56 +0300

    home page fix, added relevant info

---
+++ src/main/java/wasdev/sample/servlet/ControllerServlet.java
@@ -0,0 +1,35 @@
+package wasdev.sample.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ControllerServlet
+ */
+@WebServlet(name = "ControllerServlet.java", urlPatterns = {"/", "/home"})
+public class ControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		User user = new User();
+		user.set_name("Liudas");
+		user.set_email("mok@info.lt");
+		
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+		request.setAttribute("user", user);;
+		dispatcher.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
